64cad6b42ff9fa404344b8a05d77ec4ec3b4b813
bevilaquabruno <bbbevilaqua@gmail.com>
2021-08-09 20:35:09 -0300

    add banco.txt and append true

--- farmacia/src/testesFarmacia/TesteEntradaSaidaEscreverArquivo.java
+++ farmacia/src/testesFarmacia/TesteEntradaSaidaEscreverArquivo.java
@@ -8,7 +8,7 @@ public class TesteEntradaSaidaEscreverArquivo {
 	public static void main(String[] args) throws IOException {
 		File f = new File("C:/bruno/UNC/2_fase/desenvolvimento_software/banco.txt");
 		FileOutputStream fos = new FileOutputStream(f, true);
-		String txt = "Teste bruno bevilaqua";
+		String txt = "Teste bruno bevilaqua \n";
 		fos.write(txt.getBytes());
 		
 		fos.close();
