e076774e3eb90620f7943fd80ff454c25ca6b777
bevilaquabruno <bbbevilaqua@gmail.com>
2021-08-23 20:21:11 -0300

    removed unused imports

--- farmacia/src/farmacia/Farmaceutico.java
+++ farmacia/src/farmacia/Farmaceutico.java
@@ -1,12 +1,7 @@
 package farmacia;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Farmaceutico extends Funcionario{
 	/* Attributes */
